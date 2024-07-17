b9836c01c2716dd5e0bb625d2f59867c27810775
Sebastian <sebastian@schonrocks.com>
2024-07-17 16:27:02 +0100

    Step2 Works

--- server/server.go
+++ server/server.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/gorilla/sessions"
 	"github.com/srinathgs/mysqlstore"
 )
 
@@ -24,16 +25,46 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "<h1>This is the ant game!!!!!!!!</h1>")
 	io.WriteString(w, "<p>Get your grass <a href='/trade'>here</a></p>")
 }
-func buyGrass(w http.ResponseWriter, amt int) {
-	io.WriteString(w, "<h4>Bought: "+fmt.Sprint(amt)+"</h4>")
+func buyGrass(amt int, sess *sessions.Session) string {
+	sess.Values["totalGrass"] = amt + sess.Values["totalGrass"].(int)
+	return "Bought: " + fmt.Sprint(amt)
+
 }
-func sellGrass(w http.ResponseWriter, amt int) {
-	io.WriteString(w, "<h4>Sold: "+fmt.Sprint(amt)+"</h4>")
+func sellGrass(amt int, sess *sessions.Session) string {
+	totalGrass := sess.Values["totalGrass"].(int)
+	if totalGrass >= amt {
+		sess.Values["totalGrass"] = totalGrass - amt
+		return "Sold: " + fmt.Sprint(amt)
+	} else {
+		return "Failed to sell"
+	}
+
 }
 func handleTrade(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+	session, _ := store.Get(r, "antsTrading")
+	if session.Values["totalGrass"] == nil {
+		session.Values["totalGrass"] = 0
+	}
 
-	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
+	fmt.Printf("%s: got /trade request\n", ctx.Value(keyServerAddr))
+	var message string
+	if r.URL.Query().Get("action") == "buy_grass" {
+		q, err := strconv.Atoi(r.FormValue("quantityOfGrass"))
+		if err == nil {
+			message = buyGrass(q, session)
+		}
+	} else if r.URL.Query().Get("action") == "sell_grass" {
+		q, err := strconv.Atoi(r.FormValue("quantityOfGrass"))
+		if err == nil {
+			message = sellGrass(q, session)
+		}
+	}
+	err := session.Save(r, w)
+	fmt.Printf("%#v\n", session)
+	if err != nil {
+		fmt.Println("session.save error = ", err)
+	}
 	io.WriteString(w, "<h1>hello Bozo </h1>"+
 		"<p>You are 69 years old</p>"+
 		"<p>Are you trading today?</p>")
@@ -43,18 +74,9 @@ func handleTrade(w http.ResponseWriter, r *http.Request) {
 		"  <button formaction='?action=buy_grass'>Buy</button>"+
 		"  <button formaction='?action=sell_grass'>Sell</button>"+
 		"</form>")
+	io.WriteString(w, "<h4>"+message+"</h4>")
 
-	if r.URL.Query().Get("action") == "buy_grass" {
-		q, err := strconv.Atoi(r.FormValue("quantityOfGrass"))
-		if err == nil {
-			buyGrass(w, q)
-		}
-	} else if r.URL.Query().Get("action") == "sell_grass" {
-		q, err := strconv.Atoi(r.FormValue("quantityOfGrass"))
-		if err == nil {
-			sellGrass(w, q)
-		}
-	}
+	io.WriteString(w, "<h4>Total grass: "+fmt.Sprint(session.Values["totalGrass"])+"</h4>")
 
 	io.WriteString(w, "<pre>Get:")
 	io.WriteString(w, fmt.Sprint(r.URL.Query()))
@@ -63,15 +85,7 @@ func handleTrade(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "<pre>Post:")
 	io.WriteString(w, fmt.Sprint(r.Form))
 	io.WriteString(w, "</pre>")
-}
 
-func sessTest(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "foobar")
-	session.Values["bar"] = "baz"
-	session.Values["baz"] = "foo"
-	err = session.Save(r, w)
-	fmt.Printf("%#v\n", session)
-	fmt.Println(err)
 }
 
 func main() {
@@ -81,7 +95,6 @@ func main() {
 		panic(err)
 	}
 	defer store.Close()
-	http.HandleFunc("/sesstest", sessTest)
 
 	http.HandleFunc("/", handleRoot)
 	http.HandleFunc("/trade", handleTrade)
