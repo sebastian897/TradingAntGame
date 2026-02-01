67cde46fc7add18e774e4e81ce9adcef75e35396
Sebastian Schonrock <sebastian@schonrocks.com>
2026-02-01 22:12:08 +0000

    ignore and .env

--- go.mod
+++ go.mod
@@ -2,14 +2,12 @@ module ants/server
 
 go 1.22.5
 
-require (
-	github.com/gorilla/sessions v1.3.0
-	github.com/srinathgs/mysqlstore v0.0.0-20231123182912-ffbca72c0a70
-)
+require github.com/gorilla/sessions v1.3.0
 
 require (
 	github.com/a-h/templ v0.2.747
 	github.com/go-sql-driver/mysql v1.8.1
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.25.0
 )
 
--- server/server.go
+++ server/server.go
@@ -17,6 +17,7 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -254,9 +255,17 @@ func main() {
 		SameSite: http.SameSiteStrictMode,
 	}
 	store = fileStore // global interface
-	
-	db, err = sql.Open("mysql", "ants:REDACTED@tcp(127.0.0.1:3306)/ants")
+
+	err = godotenv.Load()
 	if err != nil {
+		panic(err)
+	}
+
+	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME")))
+	if err != nil {
+		panic(err)
+	}
+	if err = db.Ping(); err != nil {
 		panic(err)
 	}
 	db.SetConnMaxLifetime(time.Minute * 3)
@@ -268,7 +277,9 @@ func main() {
 	http.HandleFunc("/seb/ants/login", handleLogin)
 	http.HandleFunc("/seb/ants/register", handleRegister)
 
-	err = http.ListenAndServe("127.0.0.1:8080", nil)
+	serverUrl := os.Getenv("SERVER_URL")
+	fmt.Printf("Listening on %s\n", serverUrl)
+	err = http.ListenAndServe(serverUrl, nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
 	} else if err != nil {
